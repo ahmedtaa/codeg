74e9d4ea5823daf76fc5d8a9489c114e30ab8eb4
ahmedta <kalkolty@hotmail.com>
2019-03-10 00:56:03 +0200

    adding semantic-ui theme

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,39 +1,14 @@
-import React, { Component } from 'react';
-class App extends Component {
-    
-    render() { 
-        return ( 
-            <div>
-            
+import React, { Component } from "react";
+import Layout from "./Layout/Layout";
 
-<div class="ui secondary pointing menu">
-<a class="logo item">
- startup
-</a>
-<a class="active item">
- <i class="flaticon-home"></i> Home
-</a>
-<a class="item">
- <i class="flaticon-mail"></i> Messages
-</a>
-<a class="item">
- <i class="flaticon-heart"></i> Friends
-</a>
-<div class="right menu">
- <div class="item">
-   <div class="ui icon input">
-     <input placeholder="Search..." type="text">
-     <i class="flaticon-position link icon"></i>
-   </div>
- </div>
- <a class="ui item">
-   Logout
- </a>
-</div>
-</div>
-            </div>
-         );
-    }
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout />
+      </div>
+    );
+  }
 }
- 
+
 export default App;
---
+++ client/src/components/Layout/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+const NavBar = () => (
+  <Menu>
+    <Menu.Menu>
+      <Menu.Item>Main Menu</Menu.Item>
+    </Menu.Menu>
+  </Menu>
+);
+
+export default NavBar;
