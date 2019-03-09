b362602d2bec524d492c3322a95f789f48b88dbf
ahmedtaa <a.alrakhawy@gmail.com>
2019-03-10 01:04:25 +0200

    adding semantic-ui theme

--- client/src/components/Layout/NavBar.js
+++ client/src/components/Layout/NavBar.js
@@ -4,7 +4,7 @@ import { Menu } from "semantic-ui-react";
 const NavBar = () => (
   <Menu>
     <Menu.Menu>
-      <Menu.Item>Main Menu</Menu.Item>
+      <Menu.Item>Main menu</Menu.Item>
     </Menu.Menu>
   </Menu>
 );
